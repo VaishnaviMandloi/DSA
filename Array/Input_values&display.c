acd73ff0c7073788f771f423969e42adabd3b308
VaishnaviMandloi <98760492+VaishnaviMandloi@users.noreply.github.com>
2023-05-28 20:18:24 +0530

    Update Input_values&display.c

--- Array/Input_values&display.c
+++ Array/Input_values&display.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+void main()
+{
+  int arr[5] , i;
+  for(i=0;i<5;i++){
+    printf("enter the value of element %d : ",i+1);
+    scanf("%d",&arr[i]);
+  }
+  printf("the array elements are :");
+  for(i=0;i<5;i++)
+  {
+    printf("%d\t",arr[i]);
+  }
+}
