f915ae97e65bf66819969d3c4d1e02908d794ec7
VaishnaviMandloi <98760492+VaishnaviMandloi@users.noreply.github.com>
2023-06-12 15:46:52 +0530

    Create callByReference.c

---
+++ Pointer/callByReference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void func(int *a , float *b);
+
+int main()
+{
+    int a = 10;
+    float b =15.5;
+    printf("value before calling function : a = %d and b = %f\n", a , b);
+    func(&a,&b);
+    printf("value after calling function : a = %d and b = %f\n", a , b);
+
+    return 0;
+}
+
+void func(int *a , float *b){
+    (*a)++;
+    (*b)++;
+    printf("value inside the function : a = %d and b = %f\n", *a , *b);
+}
