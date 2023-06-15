7339d584946e701f1e76838db4b5d1e08675e7a7
VaishnaviMandloi <98760492+VaishnaviMandloi@users.noreply.github.com>
2023-06-15 22:31:36 +0530

    Create Passing StructureVariableAsArgument.c

---
+++ Structure/Passing StructureVariableAsArgument.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student{
+    char name[20];
+    int rollno;
+    int marks;
+};
+
+void display(struct student);
+
+int main()
+{
+    struct student stu1 ={"ram", 1 , 56} , stu2={"sham das", 2 , 86};
+    
+    display(stu1);
+    display(stu2);
+
+    return 0;
+}
+
+void display(struct student stu){
+    printf("name : %s\troll no. : %d\tmarks : %d\n",stu.name,stu.rollno,stu.marks);
+}
