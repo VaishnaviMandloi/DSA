18d9973139ea22760f2d4f68394c17355d743e62
VaishnaviMandloi <98760492+VaishnaviMandloi@users.noreply.github.com>
2023-07-04 17:25:59 +0530

    Update doublylinkedlist.c

--- Linked lists/doublylinkedlist.c
+++ Linked lists/doublylinkedlist.c
@@ -3,9 +3,9 @@
 
 struct node{
     struct node *next;
-    int data;
+    int info;
     struct node *prev;
-}
+};
 
 struct node *create_list(struct node*start);
 void display(struct node *start);
@@ -15,7 +15,7 @@ struct node *addatend(struct node*start, int data);
 struct node *addafter(struct node*start, int data, int item);
 struct node *addbefore(struct node*start, int data,int item);
 struct node *del(struct node*start,int data);
-sturct node *reverse(struct node*start);
+struct node *reverse(struct node*start);
 
 int main()
 { 
@@ -88,7 +88,24 @@ int main()
     }
 }
 
-struct node *create_list(struct node*start);
+struct node *create_list(struct node*start){
+    int i, data,n;
+    printf("\nenter the number upto which you want to add elements :");
+    scanf("%d",&n);
+    start=NULL;
+    if(n==0){
+        return start;
+    }
+    printf("\nenter the element 1 : ");
+    scanf("%d",&data);
+    start =addtoempty(start,data);
+    for (int i = 2; i <= n; i++) {
+        printf("\nenter the element %d : ",i);
+        scanf("%d",&data);
+        start=addatend(start,data);
+    }
+    return start;
+}
 
 void display(struct node *start){
     struct node*p;
@@ -111,6 +128,7 @@ void display(struct node *start){
 struct node *addtoempty(struct node *start , int data){
     struct node*tmp;
     tmp=(struct node*)malloc(sizeof(struct node));
+    tmp->info=data;
     tmp->prev=NULL;
     tmp->next=NULL;
     start=tmp;
@@ -129,7 +147,7 @@ struct node *addatbeg(struct node*start,int data){
 }
 
 struct node *addatend(struct node*start, int data){
-    struct node*tmo;
+    struct node*tmp,*p;
     tmp=(struct node*)malloc(sizeof(struct node));
     tmp->info=data;
     p=start;
@@ -148,7 +166,7 @@ struct node *addafter(struct node*start, int data, int item){
     tmp->info=data;
     p=start;
     while(p!=NULL){
-        if(p->info=item){
+        if(p->info==item){
             tmp->next=p->next;
             tmp->prev=p;
             if(p->next!=NULL){
@@ -165,7 +183,7 @@ struct node *addafter(struct node*start, int data, int item){
 
 struct node *addbefore(struct node*start, int data,int item){
     struct node *p,*tmp;
-    if(start=NULL){
+    if(start==NULL){
         printf("list is empty\n");
         return start;
     }
@@ -189,9 +207,59 @@ struct node *addbefore(struct node*start, int data,int item){
         }
         p=p->next;
     }
-    printf("%d item not found in the list\n",item)
+    printf("%d item not found in the list\n",item);
     return start;
 }
 
-struct node *del(struct node*start,int data);
-sturct node *reverse(struct node*start);
+struct node *del(struct node*start,int data){
+    struct node*tmp;
+    if(start==NULL){
+        printf("\n List is empty\n");
+        return start;
+    }
+    if(start->info==data){
+        tmp=start;
+        start=start->next;
+        if(start->next==NULL){
+            free(tmp);
+            return start;
+        }
+        start->prev=NULL;
+        free(tmp);
+        return start;
+    }
+    tmp = start->next;
+    while(tmp->next!=NULL){
+        if(tmp->info==data){
+            tmp->prev->next=tmp->next;
+            tmp->next->prev=tmp->prev;
+            free(tmp);
+            return start;
+        }
+        tmp=tmp->next;
+    }
+    if(tmp->info=data){
+        tmp->prev->next=NULL;
+        free(tmp);
+        return start;
+    }
+    printf("element %d not found\n",data);
+    return start;
+}
+
+struct node *reverse(struct node*start){
+    struct node *p1,*p2;
+    p1 = start;
+    p2 = p1->next;
+    p1->next=NULL;
+    p1->prev=p2;
+    while(p2!=NULL){
+        p2->prev=p2->next;
+        p2->next=p1;
+        p1=p2;
+        p2=p2->prev;
+    }
+    start=p1;
+    printf("list reversed\n");
+    return start;
+}
